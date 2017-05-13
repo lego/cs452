fadf3c69de6dea470a88d567640028fa76ffc11e
Joey Pereira <joey@pereira.io>
2017-05-13 17:41:12 -0400

    Added userland compilation

---
+++ src/kernel_interface.c
@@ -0,0 +1,19 @@
+#include <kernel.h>
+
+int Create( int priority, void (*code)( ) ) {
+  return 0;
+}
+
+int MyTid( ) {
+  return 0;
+}
+
+int MyParentTid( ) {
+  return 0;
+}
+
+void Pass( ) {
+}
+
+void Exit( ) {
+}
---
+++ userland/test_task.c
@@ -0,0 +1,7 @@
+#include <test_task.h>
+#include <bwio.h>
+
+void test_task() {
+  bwputstr(COM2, "Hello world from test!");
+  return;
+}
