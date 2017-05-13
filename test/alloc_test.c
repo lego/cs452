01d8077e04ff0c8e6f801213e1d14112e933598b
Joey Pereira <joey@pereira.io>
2017-05-13 18:44:20 -0400

    Added tests folder

---
+++ test/alloc_test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <assert.h>
+#include <alloc.h>
+
+
+int main() {
+  return 0;
+}
--- userland/basic_task.c
+++ userland/basic_task.c
@@ -1,8 +1,12 @@
 #include <kernel.h>
+#include <ts7200.h>
+#include <bwio.h>
 #include <basic_task.h>
 #include <test_task.h>
 
 void basic_task() {
+  bwputstr(COM2, "Enter: basic_task\n");
   Create(1, &test_task);
+  bwputstr(COM2, "Exit: basic_task\n");
   return;
 }
