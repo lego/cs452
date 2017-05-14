b7fa4bfeb59078e3c5698592b281754329c88794
Terrance Niechciol <TNiechciol@gmail.com>
2017-05-14 16:06:39 -0400

    WIP trying to get interrupt handler working

--- src/main.c
+++ src/main.c
@@ -2,6 +2,17 @@
  * iotest.c
  */
 
+asm(
+  "interrupt_vector_table:\n\t"
+  "  b . @ Reset Handler\n\t"
+  "  b . @ Undefined\n\t"
+  "  b swi_handler @ SWI Handler\n\t"
+  "  b . @ Prefetch Abort\n\t"
+  "  b . @ Data Abort\n\t"
+  "  b . @ IRQ\n\t"
+  "  b . @ FIQ\n\t"
+);
+
 #include <bwio.h>
 
 typedef struct TaskState_t {
@@ -62,6 +73,13 @@ void switch_to(void* addr) {
   );
 }
 
+void swi_handler(unsigned int number) {
+  bwprintf(COM2, "C_SWI_handler: syscall %d\n\r", number);
+  asm(
+    "movs pc, lr\n\t"
+  );
+}
+
 int Create(int priority, void* code) {
   return 0;
 }
@@ -75,6 +93,9 @@ int MyParentId() {
 }
 
 void Pass() {
+  asm(
+    "swi #3"
+  );
 }
 
 void Exit() {
@@ -82,8 +103,45 @@ void Exit() {
 
 int main() {
   bwsetfifo(COM2, OFF);
+  //*((int*)0x8) = &swi_handler;
+  //*((int*)0x20) = &swi_handler;
+  //*((int*)0x28) = &swi_handler;
   bwprintf(COM2, "main: Hello, world!\n\r");
+  // TODO: This code moves the pc to swi_handler from anywhere.
+  //   Need to put that code at 0x28? Or at least somewhere reachable from 0x08
+  //   Should check what instructions are at 0x08 by default
+  //   If it's mov pc, [pc, #18], check what's at 0x08 + #18
+  asm(
+    "ldr r3, [pc, #8]\n\t"
+    "ldr	r3, [sl, r3]\n\t"
+    "mov lr, pc\n\t"
+    "mov pc, r3\n\t"
+    ".word	swi_handler(GOT)\n\t"
+  );
+  bwprintf(COM2, "main: asdf!\n\r");
+  //asm(
+  //  "mov lr, pc\n\t"
+  //  "mov pc, #8\n\t"
+  //);
+  //asm(
+  //  "mov r3, #8\n\t"
+  //  "ldr	r3, [r3, #0]\n\t"
+  //  "mov lr, pc\n\t"
+  //  "mov pc, r3\n\t"
+  //);
+  //asm(
+  //  "swi #3"
+  //);
+  //Pass();
   switch_to(&taskCode);
   bwprintf(COM2, "main: Hello, returned world!\n\r");
   return 0;
 }
+asm(
+  "\n.main_data:\n\t"
+  "ldr r3, [pc, #8]\n\t"
+  "ldr	r3, [sl, r3]\n\t"
+  "mov lr, pc\n\t"
+  "mov pc, r3\n\t"
+  ".word	swi_handler(GOT)\n\t"
+);
