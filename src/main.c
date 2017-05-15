9d947e6aff83ed5770e6b21636053d3db0266cd8
Terrance Niechciol <TNiechciol@gmail.com>
2017-05-15 12:56:40 -0400

    SWI handling and syscall returns

--- src/main.c
+++ src/main.c
@@ -2,83 +2,35 @@
  * iotest.c
  */
 
-asm(
-  "interrupt_vector_table:\n\t"
-  "  b . @ Reset Handler\n\t"
-  "  b . @ Undefined\n\t"
-  "  b swi_handler @ SWI Handler\n\t"
-  "  b . @ Prefetch Abort\n\t"
-  "  b . @ Data Abort\n\t"
-  "  b . @ IRQ\n\t"
-  "  b . @ FIQ\n\t"
-);
-
 #include <bwio.h>
 
-typedef struct TaskState_t {
-  void* sl;
-  void* fp;
-  void* sp;
-  void* lr;
-  void* pc;
-} TaskState;
+typedef int (*interrupt_handler)(int);
 
-typedef struct TaskDescriptor_t {
-  TaskState state;
-} TaskDescriptor;
-
-TaskState kernelState;
-
-void switch_back() {
-  TaskState* localState = &kernelState;
-  asm(
-	  "ldr	r3, [fp, #-20]\n\t"
-    "ldr sl, [r3, #0]\n\t"
-    "ldr fp, [r3, #4]\n\t"
-    "ldr sp, [r3, #8]\n\t"
-    "ldr lr, [r3, #12]\n\t"
-    "ldr pc, [r3, #16]\n\t"
-  );
+int swi_handler(int number) {
+  bwprintf(COM2, "swi_handler: syscall %x\n\r", number & 0x0000FFFF);
+  return 5;
 }
 
-int taskCode() {
-  bwprintf(COM2, "taskCode: Hello, again!\n\r");
-  switch_back();
-  bwprintf(COM2, "taskCode: Hello, a third time!\n\r");
-  return 0;
+void* schedule(void* stack) {
+  bwprintf(COM2, "schedule: sp %x\n\r", stack);
+  return stack; // restart the same task
 }
 
-void switch_to(void* addr) {
-  bwprintf(COM2, "switchTo: %x\n\r", addr);
-  TaskState* localState = &kernelState;
-  asm(
-	  "ldr	r3, [fp, #-20]\n\t"
-    "str sl, [r3, #0]\n\t"
-    "str fp, [r3, #4]\n\t"
-    "str sp, [r3, #8]\n\t"
-    "str lr, [r3, #12]\n\t"
-    "str pc, [r3, #16]\n\t"
-  );
-  asm(
-    "mov lr, pc\n\t"
-    "ldr pc, [fp, #-24]\n\t"
-  );
-  bwprintf(COM2, "switchTo: end\n\r");
-  bwprintf(COM2,
-      "switch_to: %x\n\r           %x\n\r           %x\n\r           %x\n\r",
-      kernelState.sl,
-      kernelState.fp,
-      kernelState.sp,
-      kernelState.pc
-  );
-}
-
-void swi_handler(unsigned int number) {
-  bwprintf(COM2, "C_SWI_handler: syscall %d\n\r", number);
-  asm(
-    "movs pc, lr\n\t"
-  );
-}
+void __asm_swi_handler();
+asm(
+"\n__asm_swi_handler:\n\t"
+  "stmfd sp!, {r4-r12, lr}\n\t"
+  "ldr r0, [lr, #-4]\n\t"
+  // Save stack point of active task
+  "bl swi_handler(PLT)\n\t"
+  "stmfd sp!, {r0}\n\t"
+  "mov r0, sp\n\t"
+  "bl schedule(PLT)\n\t"
+  "mov sp, r0\n\t"
+  // Set stack point to active task
+  "ldmfd sp!, {r0, r4-r12, lr}\n\t"
+  "movs pc, lr\n\t"
+);
 
 int Create(int priority, void* code) {
   return 0;
@@ -92,9 +44,9 @@ int MyParentId() {
   return 0;
 }
 
-void Pass() {
+int Pass() {
   asm(
-    "swi #3"
+    "swi #3\n\t"
   );
 }
 
@@ -103,45 +55,9 @@ void Exit() {
 
 int main() {
   bwsetfifo(COM2, OFF);
-  //*((int*)0x8) = &swi_handler;
-  //*((int*)0x20) = &swi_handler;
-  //*((int*)0x28) = &swi_handler;
+  *((interrupt_handler*)0x28) = &__asm_swi_handler;
   bwprintf(COM2, "main: Hello, world!\n\r");
-  // TODO: This code moves the pc to swi_handler from anywhere.
-  //   Need to put that code at 0x28? Or at least somewhere reachable from 0x08
-  //   Should check what instructions are at 0x08 by default
-  //   If it's mov pc, [pc, #18], check what's at 0x08 + #18
-  asm(
-    "ldr r3, [pc, #8]\n\t"
-    "ldr	r3, [sl, r3]\n\t"
-    "mov lr, pc\n\t"
-    "mov pc, r3\n\t"
-    ".word	swi_handler(GOT)\n\t"
-  );
-  bwprintf(COM2, "main: asdf!\n\r");
-  //asm(
-  //  "mov lr, pc\n\t"
-  //  "mov pc, #8\n\t"
-  //);
-  //asm(
-  //  "mov r3, #8\n\t"
-  //  "ldr	r3, [r3, #0]\n\t"
-  //  "mov lr, pc\n\t"
-  //  "mov pc, r3\n\t"
-  //);
-  //asm(
-  //  "swi #3"
-  //);
-  //Pass();
-  switch_to(&taskCode);
-  bwprintf(COM2, "main: Hello, returned world!\n\r");
-  return 0;
+  int x = Pass();
+  bwprintf(COM2, "main: syscall returned %d!\n\r", x);
+  return 1;
 }
-asm(
-  "\n.main_data:\n\t"
-  "ldr r3, [pc, #8]\n\t"
-  "ldr	r3, [sl, r3]\n\t"
-  "mov lr, pc\n\t"
-  "mov pc, r3\n\t"
-  ".word	swi_handler(GOT)\n\t"
-);
