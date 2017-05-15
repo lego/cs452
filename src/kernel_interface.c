349e2dc29021f0410e2111ae65cb2a8f04b47608
Joey Pereira <joey@pereira.io>
2017-05-15 18:39:47 -0400

    bring these up

--- src/kernel_interface.c
+++ src/kernel_interface.c
@@ -4,21 +4,27 @@
 
 int Create(int priority, void (*code)()) {
   // asm ("swi #3\n\t");
-  return context_switch(SYSCALL_CREATE, priority, code);
+  // return context_switch(SYSCALL_CREATE, priority, code);
+  asm ("swi #3\n\t");
 }
 
 int MyTid( ) {
-  return context_switch(SYSCALL_MY_TID, 0, NULL);
+  // return context_switch(SYSCALL_MY_TID, 0, NULL);
+  asm ("swi #1\n\t");
+
 }
 
 int MyParentTid( ) {
-  return context_switch(SYSCALL_MY_PARENT_TID, 0, NULL);
+  // return context_switch(SYSCALL_MY_PARENT_TID, 0, NULL);
+  asm ("swi #2\n\t");
 }
 
 void Pass( ) {
-  context_switch(SYSCALL_PASS, 0, NULL);
+  asm ("swi #4\n\t");
+  // context_switch(SYSCALL_PASS, 0, NULL);
 }
 
 void Exit( ) {
-  context_switch(SYSCALL_EXIT, 0, NULL);
+  // context_switch(SYSCALL_EXIT, 0, NULL);
+  asm ("swi #5\n\t");
 }
