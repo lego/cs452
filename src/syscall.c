e64e9d22b0b324933f727c49fd08798a2aaeb88c
Joey Pereira <joey@pereira.io>
2017-05-21 17:29:10 -0400

    remove newlines on logs

--- src/syscall.c
+++ src/syscall.c
@@ -31,43 +31,43 @@ void syscall_handle(kernel_request_t *arg) {
     syscall_receive(task, arg);
     break;
   default:
-    log_syscall("WARNING: syscall not handled. syscall_no=%d\n\r", arg->syscall);
+    log_syscall("WARNING: syscall not handled. syscall_no=%d", arg->syscall);
     break;
   }
 }
 
 
 void syscall_create(task_descriptor_t *task, kernel_request_t *arg) {
-  log_syscall("syscall=Create\n\r");
+  log_syscall("syscall=Create");
   syscall_create_arg_t *create_arg = arg->arguments;
   task_descriptor_t *new_task = td_create(ctx, task->tid, create_arg->priority, create_arg->entrypoint);
-  log_syscall("new task priority=%d tid=%d\n\r", create_arg->priority, new_task->tid);
+  log_syscall("new task priority=%d tid=%d", create_arg->priority, new_task->tid);
   scheduler_requeue_task(new_task);
   scheduler_requeue_task(task);
   ((syscall_pid_ret_t *) arg->ret_val)->tid = new_task->tid;
 }
 
 void syscall_my_tid(task_descriptor_t *task, kernel_request_t *arg) {
-  log_syscall("syscall=MyTid ret=%d\n\r", task->tid);
+  log_syscall("syscall=MyTid ret=%d", task->tid);
   scheduler_requeue_task(task);
 
   ((syscall_pid_ret_t *) arg->ret_val)->tid = task->tid;
 }
 
 void syscall_my_parent_tid(task_descriptor_t *task, kernel_request_t *arg) {
-  log_syscall("syscall=MyParentTid ret=%d\n\r", task->parent_tid);
+  log_syscall("syscall=MyParentTid ret=%d", task->parent_tid);
   scheduler_requeue_task(task);
 
   ((syscall_pid_ret_t *) arg->ret_val)->tid = task->parent_tid;
 }
 
 void syscall_pass(task_descriptor_t *task, kernel_request_t *arg) {
-  log_syscall("syscall=Pass\n\r");
+  log_syscall("syscall=Pass");
   scheduler_requeue_task(task);
 }
 
 void syscall_exit(task_descriptor_t *task, kernel_request_t *arg) {
-  log_syscall("syscall=Exit\n\r");
+  log_syscall("syscall=Exit");
   scheduler_exit_task();
 }
 
@@ -82,7 +82,7 @@ void copy_msg(task_descriptor_t *src_task, task_descriptor_t *dest_task) {
 }
 
 void syscall_send(task_descriptor_t *task, kernel_request_t *arg) {
-  log_syscall("syscall=Send\n\r");
+  log_syscall("syscall=Send");
   task->state = STATE_RECEIVE_BLOCKED;
   syscall_message_t *msg = arg->arguments;
   task_descriptor_t *target_task = &ctx->descriptors[msg->tid];
@@ -103,7 +103,7 @@ void syscall_send(task_descriptor_t *task, kernel_request_t *arg) {
 }
 
 void syscall_receive(task_descriptor_t *task, kernel_request_t *arg) {
-  log_syscall("syscall=Receive\n\r");
+  log_syscall("syscall=Receive");
   task->state = STATE_SEND_BLOCKED;
   syscall_message_t *msg = arg->ret_val;
 
