64cde3e8f665b017313eba5094c9d7d80022d0b0
Joey Pereira <joey@pereira.io>
2017-05-23 14:04:08 -0400

    Revert "Old entry task"
    This reverts commit c04131bfe4ad8993e7edb96ff60edb832459e672.

--- userland/k2_entry_task.c
+++ userland/k2_entry_task.c
@@ -1,5 +1,7 @@
+#include <basic.h>
 #include <bwio.h>
 #include <entry_task.h>
+#include <nameserver.h>
 #include <kernel.h>
 #include <ts7200.h>
 
@@ -13,18 +15,17 @@ void child_task() {
   result = Receive(&from_tid, buf, 100);
   bwprintf(COM2, "T1 Received message. result=%d source_tid=%d msg=%s\n\r", result, from_tid, buf);
 
+  Reply(from_tid, NULL, 0);
+
   bwprintf(COM2, "T1 Getting parent tid.\n\r");
   int parent_tid = MyParentTid();
 
-  debugger();
-
   bwprintf(COM2, "T1 Sending message to tid=%d msg=%s\n\r", parent_tid, bye);
   result = Send(parent_tid, bye, 4, NULL, 0);
-  result = Receive(&from_tid, buf, 100);
 
   bwprintf(COM2, "T1 Child task exiting\n\r");
+  Exit();
 }
-
 
 void k2_entry_task() {
   int new_task_id;
@@ -34,9 +35,7 @@ void k2_entry_task() {
   char *hello = "HELLO";
 
   bwprintf(COM2, "T0 Task started\n\r");
-
-  new_task_id = Create(PRIORITY_LOW, &child_task);
-
+  new_task_id = Create(2, &child_task);
   bwprintf(COM2, "T0 Created tid=%d\n\r", new_task_id);
 
   bwprintf(COM2, "T0 Sending message. tid=%d msg=%s\n\r", new_task_id, hello);
@@ -46,8 +45,8 @@ void k2_entry_task() {
   bwprintf(COM2, "T0 Going to receive message.\n\r");
   result = Receive(&from_tid, buf, 100);
   bwprintf(COM2, "T0 Received message. result=%d source_tid=%d msg=%s\n\r", result, from_tid, buf);
+  Reply(from_tid, NULL, 0);
 
   bwprintf(COM2, "T0 Entry task exiting\n\r");
-
   Exit();
 }
