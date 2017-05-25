1474a940c5e6b0982e73131fea5a3520b715c735
Terrance Niechciol <TNiechciol@gmail.com>
2017-05-25 16:39:37 -0400

    Whoops, swap user code order

--- userland/entry_task.c
+++ userland/entry_task.c
@@ -4,6 +4,15 @@
 #include <kernel.h>
 #include <ts7200.h>
 
+void child_task() {
+  int my_tid = MyTid();
+  int my_parent_tid = MyParentTid();
+  bwprintf(COM2, "MyTid=%d MyParentTid=%d\n\r", my_tid, my_parent_tid);
+  Pass();
+  bwprintf(COM2, "MyTid=%d MyParentTid=%d\n\r", my_tid, my_parent_tid);
+  Exit();
+}
+
 void entry_task() {
   int new_task_id;
   new_task_id = Create(PRIORITY_HIGHEST, &child_task);
@@ -15,15 +24,5 @@ void entry_task() {
   new_task_id = Create(PRIORITY_LOW, &child_task);
   bwprintf(COM2, "Created: %d\n\r", new_task_id);
   bwprintf(COM2, "FirstUserTask: exiting\n\r");
-  Exit();
-}
-
-
-void child_task() {
-  int my_tid = MyTid();
-  int my_parent_tid = MyParentTid();
-  bwprintf(COM2, "MyTid=%d MyParentTid=%d\n\r", my_tid, my_parent_tid);
-  Pass();
-  bwprintf(COM2, "MyTid=%d MyParentTid=%d\n\r", my_tid, my_parent_tid);
   Exit();
 }
