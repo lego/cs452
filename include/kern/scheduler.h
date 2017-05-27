1a25fff20c89c72caa2017aa822c06b53820ecb1
Joey Pereira <joey@pereira.io>
2017-05-27 03:43:15 -0400

    Performance improvements

--- include/kern/scheduler.h
+++ include/kern/scheduler.h
@@ -4,6 +4,14 @@
 #include <kern/task_descriptor.h>
 #include <kern/kernel_request.h>
 
+#define MIN_PRIORITY 0
+#define MAX_PRIORITY 31
+
+extern unsigned long int priotities_ready;
+
+extern task_descriptor_t *ready_queues[MAX_PRIORITY + 1];
+extern task_descriptor_t *ready_queues_end[MAX_PRIORITY + 1];
+
 /*
  * functionality for scheduling and swapping the active task as
  * part of scheduling
@@ -11,11 +19,15 @@
 
 // NOTE: the below constructs refer to task switching
 
+#ifndef SCHEDULER_INLINE
+#define SCHEDULER_INLINE INLINE
+#endif
+
 /**
  * Initializes scheduler state
  * - currently global variables for x86 :(
  */
-void scheduler_init();
+SCHEDULER_INLINE void scheduler_init();
 void scheduler_arch_init();
 
 /**
@@ -56,12 +68,11 @@ void scheduler_requeue_task(task_descriptor_t *task);
 /**
  * Checks if there are any other tasks to schedule
  */
-bool scheduler_any_task();
-
+#define scheduler_any_task() !!priotities_ready
 /**
  * Puts a task back onto the ready queue
  */
-task_descriptor_t *scheduler_next_task();
+SCHEDULER_INLINE task_descriptor_t *scheduler_next_task();
 
 /**
  * Gets the count of tasks currently on the ready queue
