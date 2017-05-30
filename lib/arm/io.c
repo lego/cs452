cdcb78aebcab39d567cfdf925fb03527012e6116
Joey Pereira <joey@pereira.io>
2017-05-30 15:04:25 -0400

    Add clobber list

--- lib/arm/io.c
+++ lib/arm/io.c
@@ -38,11 +38,8 @@ void io_init() {
 
 void io_enable_caches() {
   asm volatile (
-    // save r0 before stomping on it
-    "stmfd sp!, {r0}\n\t"
     "mov r0, #0\n\t"
     // invalidates I-Cache and D-Cache, see arm-920t (ARM 920T technical reference), pg. 312
-    "mov r0, #0\n\t"
     "mcr p15, 0, r0, c7, c7, 0\n\t"
 
     // pull value from coprocessor
@@ -54,13 +51,11 @@ void io_enable_caches() {
     // store new value into coprocessor
     "mcr p15, 0, r0, c1, c0, 0\n\t"
     // save r0 before stomping on it
-    "ldmfd sp!, {r0}\n\t"
-  );
+    : : : /* Clobber list */ "r0");
 }
 
 void io_disable_caches() {
   asm volatile (
-    "stmfd sp!, {r0}\n\t"
     // pull value from coprocessor
     "mrc p15, 0, r0, c1, c0, 0\n\t"
     // disable I-cache (bit 12) ep93xx-user-guide section 2.2.3.3.1, page 43
@@ -69,8 +64,7 @@ void io_disable_caches() {
     "bic r0, r0, #4\n\t"
     // store new value into coprocessor
     "mcr p15, 0, r0, c1, c0, 0\n\t"
-    "ldmfd sp!, {r0}\n\t"
-  );
+    : : : /* Clobber list */ "r0");
 }
 
 io_time_t io_get_time() {
