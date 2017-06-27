34f1d7c4844cd64e533d4aa741946b621986c4cc
Joey Pereira <joey@pereira.io>
2017-06-27 17:34:21 -0400

    Add track test; track b

--- track/track_data.h
+++ track/track_data.h
@@ -7,4 +7,6 @@
 // The track initialization functions expect an array of this size.
 #define TRACK_MAX 144
 
+void init_tracktest(track_node *track);
 void init_tracka(track_node *track);
+void init_trackb(track_node *track);
