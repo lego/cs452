0fb8a3094539ba576bfec2eca70b46b9ba257d5f
Joey Pereira <joey@pereira.io>
2017-06-27 12:22:37 -0400

    Revert "lol"
    This reverts commit 5e9d396d34509d894f824d6bd5c1c339cc665340.

--- track/track_data.h
+++ track/track_data.h
@@ -5,7 +5,7 @@
 #include "track_node.h"
 
 // The track initialization functions expect an array of this size.
-#define TRACK_MAX 144
+#define TRACK_MAX 8
 
-void init_tracka(track_node *track);
-void init_tracka_route_table(track_node *track, track_edge *(*route_table)[TRACK_MAX][TRACK_MAX]);
+void init_tracktest(track_node *track);
+void init_tracktest_route_table(track_node *track, track_edge *(*route_table)[TRACK_MAX][TRACK_MAX]);
