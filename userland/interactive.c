1b5a9d36e76dfd023e27252983905c9921938a9b
Joey Pereira <joey@pereira.io>
2017-06-28 02:18:10 -0400

    attempt E9 stop

--- userland/interactive.c
+++ userland/interactive.c
@@ -582,6 +582,9 @@ void sensor_saver() {
   GetPath(&p, B1, E14);
   int E14_dist = p.dist;
 
+  GetPath(&p, E14, Name2Node("E9"));
+  int E14_E9_dist = p.dist;
+
   while (true) {
     ReceiveS(&sender, req);
     switch (req.type) {
@@ -596,7 +599,7 @@ void sensor_saver() {
           int curr_time = Time();
           sensor_reading_timestamps[req.argc] = curr_time;
           if (req.argc == C10) {
-            int remaining_mm = 280 + 56 + 30;
+            int remaining_mm = 280 + 56 + 30 + E14_E9_dist;
             int velocity = 485;
             int wait_ticks = remaining_mm * 100 / velocity;
             Delay(wait_ticks);
